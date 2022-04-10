4257de924f98cb2231aad1a92eb84cdb99436145
SHEELA NEHA <96616259+Sheela-Neha-07@users.noreply.github.com>
2022-04-10 19:33:03 +0530

    Update area of a circle.py

--- area of a circle.py
+++ area of a circle.py
@@ -1,3 +1,40 @@
+#task1
 radius= float(input())
 area= float(3.14*(radius**2))
 print(area)
+
+
+#task 2
+file_name= str(input())
+len_file= len(file_name)
+b= int(len_file)
+
+extension= file_name[b-2:b+1]
+if extension=="py":
+    print ("python")
+else:
+    print ("N
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           ot python")
+ 
